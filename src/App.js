1833bb464c4b229dee02cae13961c67ee1956453
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-03-25 06:26:28 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <LoadComment></LoadComment>
     </div>
   );
 }
+
+function LoadComment() { 
+  const [comments, setComments] = useState([]);
+  useEffect(() => { 
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(response => response.json())
+      .then(data => setComments(data))
+  }, []);
+  return (
+    <div>
+      <h2>{comments.length}</h2>
+      {
+        comments.map(comment => <Comment id={ comment.id } name={ comment.name } email={comment.email} body={ comment.body }></Comment>)
+      }
+    </div>
+  )
+}
+function Comment(props) {
+  return (
+    <div>
+      <h1>Id: { props.id }</h1>
+      <h2>Name: { props.name }</h2>
+      <h4>Email: {props.email}</h4>
+      <em>Body: { props.body }</em>
+    </div>
+  )
+}
+
 
 function Counter() { 
   const [count, setCounter] = useState(10);
